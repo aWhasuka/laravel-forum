dcb2e8b38d15cfa48f2c7ff8ea8cb8960f0458ff
aWhasuka <aWhasuka@foxmail.com>
2020-05-21 22:36:41 +0800

    用户头像上传

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,9 +5,15 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     public function show(User $user)
     {
         return view('users.show', compact('user'));
@@ -18,10 +24,19 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request, User $user, ImageUploadHandler $uploader)
     {
-        dd($request->avatar);
-        $user->update($request->all());
+        $data = $request->all();
+
+        if ($request->avatar){
+            $result = $uploader->save($request->avatar, 'avatars', $user->id, 416);
+
+            if($result){
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
