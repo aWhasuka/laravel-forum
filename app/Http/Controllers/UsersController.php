accd8099a82c5fe452ce033b1f73b14fd8c0fc17
awhasuka <awhasuka@foxmail.com>
2020-05-22 12:46:07 +0800

    授权策略访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,6 +11,17 @@ class UsersController extends Controller
 {
     public function __construct()
     {
+        /**
+         * 通过 except 方法来设定 指定动作 不使用 Auth 中间件进行过滤，意为 —— 除了此处指定的动作以外，
+         * 所有其他动作都必须登录用户才能访问，类似于黑名单的过滤机制。相反的还有 only 白名单方法，将只过滤指定动作。
+         * 我们提倡在控制器 Auth 中间件使用中，首选 except 方法，这样的话，当你新增一个控制器方法时，默认是安全的，此为最佳实践
+         */
+
+        /**
+         *  except 指定的方法，不检测是否登录
+         *  only   指定的方法，检测是否登录
+         *  如不设置 则全部需要登录才可访问
+         */
         $this->middleware('auth', ['except' => ['show']]);
     }
 
@@ -21,11 +32,20 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        /**
+         * 这里 update 是指授权类里的 update 授权方法，$user 对应传参 update 授权方法的第二个参数。
+         * 正如上面定义 update 授权方法时候提起的，调用时，默认情况下，
+         * 我们 不需要 传递第一个参数，也就是当前登录用户至该方法内，因为框架会 自动 加载当前登录用户。
+         */
+        $this->authorize('update', $user);
+
         return view('users.edit', compact('user'));
     }
 
     public function update(UserRequest $request, User $user, ImageUploadHandler $uploader)
     {
+        $this->authorize('update', $user);
+
         $data = $request->all();
 
         if ($request->avatar){
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,13 +3,13 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
+use Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
     /**
      * The policy mappings for the application.
-     *
+     * 注册授权策略
      * @var array
      */
     protected $policies = [
@@ -18,13 +18,17 @@ class AuthServiceProvider extends ServiceProvider
 
     /**
      * Register any authentication / authorization services.
-     *
+     * 自动授权默认会假设 Model 模型文件直接存放在 app 目录下，鉴于我们已将模型存放目录修改为 app/Models，接下来还需自定义自动授权注册的规则，修改 boot() 方法
      * @return void
      */
     public function boot()
     {
         $this->registerPolicies();
 
-        //
+        // 修改策略自动发现的逻辑
+        Gate::guessPolicyNamesUsing(function ($modelClass) {
+            // 动态返回模型对应的策略名称，如：// 'App\Model\User' => 'App\Policies\UserPolicy',
+            return 'App\Policies\\'.class_basename($modelClass).'Policy';
+        });
     }
 }
