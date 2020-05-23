cd3dd4381cf3a94fe1f2da8eea365dcc9c40e34a
aWhasuka <aWhasuka@foxmail.com>
2020-05-23 15:35:12 +0800

    编辑器图片上传

--- routes/web.php
+++ routes/web.php
@@ -46,3 +46,6 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+
+// 图片上传类
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
