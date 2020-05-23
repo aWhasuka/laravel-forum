7924beb5ecff41a8b0983495a698f94e1920495f
aWhasuka <aWhasuka@foxmail.com>
2020-05-23 15:25:59 +0800

    发布帖子 编辑器优化

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Topic;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
+use Auth;
 
 class TopicsController extends Controller
 {
@@ -14,12 +16,16 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
+	public function index(Request $request, Topic $topic)
 	{
 	    // 解决N+1问题，使用Eloquent 提供的 预加载功能解决
         // with() 提前加载了我们后面需要用到的关联属性 user 和 category，并做了缓存。
         // 后面即使是在遍历数据时使用到这两个关联属性，数据已经被预加载并缓存，因此不会再产生多余的 SQL 查询
-		$topics = Topic::with('user', 'category')->paginate(30);
+		// $topics = Topic::with('user', 'category')->paginate(30);
+
+        // laravel 本地作用域
+        // $request->order 是获取 URI http://larabbs.test/topics?order=recent 中的 order 参数
+        $topics = $topic->withOrder($request->order)->with('user', 'category')->paginate(20);
 		return view('topics.index', compact('topics'));
 	}
 
@@ -30,13 +36,18 @@ class TopicsController extends Controller
 
 	public function create(Topic $topic)
 	{
-		return view('topics.create_and_edit', compact('topic'));
+        $categories = Category::all();
+		return view('topics.create_and_edit', compact('topic', 'categories'));
 	}
 
-	public function store(TopicRequest $request)
+	public function store(TopicRequest $request, Topic $topic)
 	{
-		$topic = Topic::create($request->all());
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+	    // fill 将传参的键值数组填充到模型的属性中
+	    $topic->fill($request->all());
+	    $topic->user_id = Auth::id();
+	    $topic->save();
+//		$topic = Topic::create($request->all());
+		return redirect()->route('topics.show', $topic->id)->with('message', '帖子创建成功！');
 	}
 
 	public function edit(Topic $topic)
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -52,8 +52,8 @@ class UsersTableSeeder extends Seeder
 
         // 单独处理第一个用户的数据
         $user = User::find(1);
-        $user->name = 'Summer';
-        $user->email = 'summer@example.com';
+        $user->name = 'aWhasuka';
+        $user->email = 'awhasuka@foxmail.com';
         $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
         $user->save();
     }
