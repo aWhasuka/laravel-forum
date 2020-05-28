682126b1d8e515e4db7841f093f1f7098078f55c
awhasuka <awhasuka@foxmail.com>
2020-05-28 17:26:45 +0800

    用户可以发表评论

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Models\Topic;
+use App\Models\Reply;
 use App\Observers\TopicObserver;
+use App\Observers\ReplyObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,5 +29,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Topic::observe(TopicObserver::class);
+        Reply::observe(ReplyObserver::class);
     }
 }
