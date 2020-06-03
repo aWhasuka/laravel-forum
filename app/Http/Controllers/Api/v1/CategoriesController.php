17508ee89cb1c26da0577324e28e13b9c399ebb2
awhasuka <awhasuka@foxmail.com>
2020-06-03 17:20:19 +0800

    分类列表

---
+++ app/Http/Controllers/Api/v1/CategoriesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use App\Http\Resources\CategoryResource;
+
+class CategoriesController extends Controller
+{
+    public function index()
+    {
+        CategoryResource::wrap('data');
+        return CategoryResource::collection(Category::all());
+    }
+}
